cd73bfa1ccf83cfbdbcaaf3b294f14e8db9bd52f
DIALLO <ousmane.diallo1.sio@gmail.com>
2023-04-24 08:12:58 +0200

    feat: enabled hot reload with watchman trigger

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-echo "This script only works on MacOS. Run the project manually if you are using Linux or Windows.";
+# This script runs the project and uses watchman to reload Chrome when a file changes.
+
+echo "\nThis script only works on MacOS. Run the project manually if you are using Linux or Windows.";
 
 if ! which caddy > /dev/null; then
   echo "This script requires caddy to be installed.";
@@ -6,7 +8,16 @@ if ! which caddy > /dev/null; then
   exit 1;
 fi
 
+if ! which watchman > /dev/null; then
+  echo "This script requires watchman to be installed.";
+  echo "MacOS : 'brew install watchman'.";
+  exit 1;
+fi
+
 if ! echo `open -a "Google Chrome" http://localhost:80` > /dev/null; then
   open http://localhost:80;
+else
+    watchman -- trigger . reload-chrome '*' -- ./reloadChrome.sh
 fi
+
 caddy file-server --browse --listen :80;
