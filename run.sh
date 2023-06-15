de0835b6e2da24f4166a80f59691c7115f2e0ebe
DIALLO <ousmane.diallo1.sio@gmail.com>
2023-06-15 12:08:47 +0200

    fix: elements size + page count

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 # This script runs the project and uses watchman to reload Chrome when a file changes.
-# It also uses a VScode extension to compile the sass files to css.
 
 echo "\nThis script only works on MacOS. Run the project manually if you are using Linux or Windows.";
 
@@ -19,7 +18,7 @@ if ! echo `open -a "Google Chrome" http://localhost:80` > /dev/null; then
   open http://localhost:80;
 else
   watchman watch .;
-  watchman -- trigger . reload-chrome '*' -- ./reloadChrome.sh;
+  watchman -- trigger . ./* reload-chrome '*' -- ./reloadChrome.sh;
   echo "Watch list: \n";
   watchman watch-list;
 fi
